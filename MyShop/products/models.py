79d4aa88f3f4395f00b38b933ccfbda5b79a9137
Henry Lin <henryhengyi.lin@mail.utoronto.ca>
2025-01-17 15:19:15 -0800

    Setup Models and database (SQLite)

--- MyShop/products/models.py
+++ MyShop/products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    tags = models.ManyToManyField(Tag, related_name='products')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
