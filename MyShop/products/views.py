5e45d2f5da7be3ff8e527a0ab4e89e95c7ea13e7
Henry Lin <henryhengyi.lin@mail.utoronto.ca>
2025-01-17 16:30:35 -0800

    Added product page

--- MyShop/products/views.py
+++ MyShop/products/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .models import Product, Category, Tag
+from django.db.models import Q
 
 def product_list(request):
-    return HttpResponse("Hello World")
+    query = request.GET.get('query', '')
+    category = request.GET.get('category', '')
+    tags = request.GET.getlist('tags')
+
+    filters = Q()
+
+    # Apply the filters for Query, Category, and Tags
+    if query:
+        filters &= Q(description__icontains=query)
+        
+    if category:
+        filters &= Q(category__id=category)
+
+    if tags:
+        filters &= Q(tags__id__in=tags)
+
+    products = Product.objects.filter(filters).distinct()
+
+    categories = Category.objects.all()
+    all_tags = Tag.objects.all()
+
+    context = {
+        'products': products,
+        'categories': categories,
+        'tags': all_tags,
+        'selected_category': category,
+        'selected_tags': tags,
+        'query': query,
+    }
+    return render(request, 'product_list.html', context)
+
